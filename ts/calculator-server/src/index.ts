57b9cbe5583d8a44952911faaeb7e0767835f5e6
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-09-05 13:40:13 +0530

    Initialize the mcp server

--- ts/calculator-server/src/index.ts
+++ ts/calculator-server/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+ 
+// Create an MCP server
+const server = new McpServer({
+  name: "Calculator MCP Server",
+  version: "1.0.0"
+});
